fb9bee92550224441a705c4bc7a4466a93342642
Selim Çelik <selimc1761@gmail.com>
2020-05-07 22:00:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir değer gir:");
+            string yazi = Console.ReadLine();
+            if (BuyukUnluUyumu(yazi) == true)
+            {
+                Console.Write("Uygun");
+            }
+            else
+            {
+                Console.Write("Uygun DEĞİL");
+            }
+            Console.ReadLine();
+        }
+
+
+        static bool BuyukUnluUyumu(string sozluk)
+        {
+            bool ince_b = false;
+            bool kalin_b = false;
+            int diziLength = 7;
+
+
+            string[] KalinU = { "a", "ı", "u", "o", "A", "I", "U", "O" };
+            string[] Ince = { "e", "i", "ü", "ö", "E", "İ", "Ü", "Ö" };
+
+            for (int i = 0; i <= sozluk.Length - 1; i++)
+            {
+                for (int ii = 0; ii <= diziLength; ii++)
+                {
+                    if (sozluk.Substring(i, 1) == KalinU[ii]) { kalin_b = true; }
+                    if (sozluk.Substring(i, 1) == Ince[ii]) { ince_b = true; }
+                    if ((kalin_b == true) && (ince_b == true))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("buu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("buu")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("07016c1f-81d5-46c7-98f0-5ece67a24d15")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
